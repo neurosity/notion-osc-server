47b4d0c765a23891904910fb1fcb7ab99f0f7ebc
Alex Castillo <alex@neurosity.co>
2022-04-13 09:36:53 -0700

    FEAT: update default options and env variables for Crown

--- src/options.ts
+++ src/options.ts
@@ -3,38 +3,29 @@ import { config } from "dotenv";
 config();
 
 export const deviceId =
-  process.env.NEUROSITY_DEVICE_UUID ||
-  "local7cca794fb5f4675a69371e949b2";
+  process.env.NEUROSITY_DEVICE_UUID || "local7cca794fb5f4675a69371e949b2";
 
-export const channels =
-  Number(process.env.NEUROSITY_DEVICE_CHANNELS) || 8;
+export const channels = Number(process.env.NEUROSITY_DEVICE_CHANNELS) || 8;
 
-export const modelName =
-  process.env.NEUROSITY_DEVICE_MODEL_NAME || "Notion";
+export const modelName = process.env.NEUROSITY_DEVICE_MODEL_NAME || "Crown";
 
-export const modelVersion =
-  process.env.NEUROSITY_DEVICE_MODEL_VERSION || "2";
+export const modelVersion = process.env.NEUROSITY_DEVICE_MODEL_VERSION || "3";
 
 export const model = `${modelName} ${modelVersion}`;
 
 export const manufacturer =
   process.env.NEUROSITY_DEVICE_MANUFACTURER || "Neurosity, Inc";
 
-export const deviceNickname: string = getDeviceNickname(
-  deviceId,
-  modelName
-);
+export const deviceNickname: string = getDeviceNickname(deviceId, modelName);
 
 export const channelNames = (
-  process.env.NEUROSITY_DEVICE_CHANNEL_NAMES ||
-  "CP6,F6,C4,CP4,CP3,F5,C3,CP5"
+  process.env.NEUROSITY_DEVICE_CHANNEL_NAMES || "CP3,C3,F5,PO3,PO4,F6,C4,CP4"
 ).split(",");
 
 export const samplingRate =
-  Number(process.env.NEUROSITY_DEVICE_SAMPLING_RATE) || 250;
+  Number(process.env.NEUROSITY_DEVICE_SAMPLING_RATE) || 256;
 
-export const oscLocalHost =
-  process.env.NEUROSITY_OSC_LOCAL_HOST || "0.0.0.0";
+export const oscLocalHost = process.env.NEUROSITY_OSC_LOCAL_HOST || "0.0.0.0";
 
 export const oscLocalPort =
   Number(process.env.NEUROSITY_OSC_LOCAL_PORT) || 8000;
@@ -45,17 +36,11 @@ export const oscRemotePort =
 export const oscDataFormat =
   process.env.NEUROSITY_OSC_DATA_FORMAT || "brainflow";
 
-export function getDeviceNickname(
-  deviceId: string,
-  modelName: string
-): string {
+export function getDeviceNickname(deviceId: string, modelName: string): string {
   const shortId = getShortDeviceId(deviceId, 3).toUpperCase();
   return `${modelName}-${shortId}`;
 }
 
-export function getShortDeviceId(
-  deviceId: string,
-  length: number = 7
-): string {
+export function getShortDeviceId(deviceId: string, length: number = 7): string {
   return deviceId.substring(0, length);
 }
